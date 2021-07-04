b5945003417d9a1b018218edd46cf525307bd894
Alasakani Pranitha <73059159+pranithaalasakani123@users.noreply.github.com>
2021-07-04 21:14:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,81 @@
+//////////// coding challenge - 1
+
+// const mark_mass = 95;
+// const mark_height = 1.88;
+// const john_mass = 85;
+// const john_height = 1.76;
+
+// mark__BMI = mark_mass / (mark_height * mark_height);
+// console.log(mark__BMI)
+
+// john__BMI = john_mass / (john_height * john_height);
+// console.log(john__BMI)
+
+// console.log(mark__BMI > john__BMI);
+/*
+//////////////////////////////Synopsis
+const firstName = 'pranitha';
+const job = 'student';
+const birthYear = 2001;
+const year = 2070;
+
+const pranitha = "I'm " + firstName + ', a ' + job + '.' + ' my age is ' + (year - birthYear);
+
+console.log(pranitha);
+
+const praniNew = `i'm ${firstName},a ${job},i'm ${year - birthYear} year old!`;
+console.log(praniNew)
+
+console.log(`just a regular string....`)
+
+console.log('String with \n\
+ is a \n\
+ lines ');
+
+console.log(`string
+ is a
+ lines`);
+
+
+const age = 13;
+const isOldEnough = age >= 18;
+
+if (age >= 18) {
+    console.log(`your age is ${age},so you can apply`);
+} else {
+    const year = 18 - age;
+    console.log(`sorry your age is not sufficient to ride a car  for ${year}`)
+}
+
+const birth = 2012;
+let century;
+if (birth <= 2000) {
+    century = 20;
+}
+else {
+    century = 21;
+}
+
+console.log(century);
+
+
+// coding challenge - 2
+
+const mark_mass = 95;
+const mark_height = 1.88;
+const john_mass = 85;
+const john_height = 1.76;
+
+mark__BMI = mark_mass / (mark_height * mark_height);
+
+
+john__BMI = john_mass / (john_height * john_height);
+
+
+if (mark__BMI > john__BMI) {
+    console.log(`mark bmi (${mark__BMI}) is greater than john (${john__BMI})`)
+} else {
+    console.log(`john bmi  (${john__BMI}) is greater than mark (${mark__BMI})`)
+}
+
+ */
